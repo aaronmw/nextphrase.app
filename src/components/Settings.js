16b2657896c51687f7b6539aedb300c2ec85c1ea
Aaron Wright <aaronmw@gmail.com>
2017-02-23 09:47:12 -0500

    WIP

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,6 +1,7 @@
 import React, { Component, PropTypes } from "react";
 import styled from "styled-components";
-import { GameBoard, GameHeader, GameButton, GameContent } from "./GameElements";
+import { GameBoard, GameHeader, GameContent } from "./GameElements";
+import lists from "../lists";
 
 const SettingsContent = styled(GameContent)`
   border: 1px solid ${ props => props.theme.secondary };
@@ -8,9 +9,26 @@ const SettingsContent = styled(GameContent)`
   overflow: auto;
 `;
 
+const ListOption = styled.div`
+  ${ props => props.selected ? `
+    background: green;
+  ` : `
+    background: red;
+  `}
+`;
+
 class Settings extends Component {
   render() {
     const { selectedLists, onTouchSave } = this.props;
+
+    let availableLists = [];
+    Object.keys(lists).forEach(value => {
+      availableLists.push(
+        <ListOption selected={ selectedLists.indexOf(value) !== -1 ? true : false }>
+          {value}
+        </ListOption>
+      );
+    });
 
     return (
       <GameBoard>
@@ -19,7 +37,7 @@ class Settings extends Component {
           onTouchButton={onTouchSave}
         />
         <SettingsContent>
-          Settings!
+          {availableLists}
         </SettingsContent>
       </GameBoard>
     );
