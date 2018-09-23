82bd5e23b8aac17a36a74aac86019f3d28ba9a5e
Aaron Wright <aaronmw@gmail.com>
2018-09-23 17:53:23 -0400

    Add cache control

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { ThemeProvider } from "styled-components";
 import App from './components/App';
-import './index.css';//
+import './index.css';
 
 const THEME = {
   borderWidth: '4px',
