f1b8829fdd0b36ab0718a10f7427150184ad51ef
Aaron Wright <aaronmw@gmail.com>
2023-07-15 19:35:07 -0400

    Updates

---
+++ precache-manifest.80975f477c0354ad2db8519d4902ef34.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "26a9103f7ce728667d9e",
+    "url": "/whatthephrase/static/js/main.bce51a7c.chunk.js"
+  },
+  {
+    "revision": "eba66d586711bb203490",
+    "url": "/whatthephrase/static/js/runtime~main.eeff11b8.js"
+  },
+  {
+    "revision": "eddc8a145bb422bdfb2d",
+    "url": "/whatthephrase/static/js/2.f65fa6d5.chunk.js"
+  },
+  {
+    "revision": "b5cfe37577137d1abd032680d7e90c74",
+    "url": "/whatthephrase/index.html"
+  }
+];
